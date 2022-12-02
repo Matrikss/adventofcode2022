b766fc1476cc8706bf4676778314950b1d6c1625
Bruno Loureiro <matrikss@gmail.com>
2022-12-02 18:21:24 +0000

    add day 2 part 2

--- day2.go
+++ day2.go
@@ -14,7 +14,7 @@ func check(e error) {
 
 func main() {
 
-	combination := map[string]int{
+	combination1 := map[string]int{
 		"A X": 1 + 3,
 		"A Y": 2 + 6,
 		"A Z": 3 + 0,
@@ -26,16 +26,30 @@ func main() {
 		"C Z": 3 + 3,
 	}
 
+	combination2 := map[string]int{
+		"A X": 3 + 0,
+		"A Y": 1 + 3,
+		"A Z": 2 + 6,
+		"B X": 1 + 0,
+		"B Y": 2 + 3,
+		"B Z": 3 + 6,
+		"C X": 2 + 0,
+		"C Y": 3 + 3,
+		"C Z": 1 + 6,
+	}
+
 	input, err := os.ReadFile("./input/input2.txt")
 	check(err)
 
 	lines := strings.Split(string(input), "\r\n")
 
-	points := 0
+	points1 := 0
+	points2 := 0
 	for _, line := range lines {
-		points += combination[line]
+		points1 += combination1[line]
+		points2 += combination2[line]
 	}
 
-	fmt.Println("part 1: ", points)
-	fmt.Println("part 2: ", 0)
+	fmt.Println("part 1: ", points1)
+	fmt.Println("part 2: ", points2)
 }
