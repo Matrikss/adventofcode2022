0cd1f520940f52a128256b830c01de9afe8b35b7
Bruno Loureiro <matrikss@gmail.com>
2022-12-03 19:51:25 +0000

    add day 3 part 2

--- day3.go
+++ day3.go
@@ -13,6 +13,16 @@ func check(e error) {
 	}
 }
 
+func join_multi(s [][]string) string {
+	res := ""
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < len(s[i]); j++ {
+			res += s[i][j]
+		}
+	}
+	return res
+}
+
 func main() {
 
 	priorities := map[string]int{}
@@ -35,15 +45,24 @@ func main() {
 
 	lines := strings.Split(string(input), "\r\n")
 
-	sum := 0
+	sum1 := 0
 	for _, line := range lines {
 		f_half := line[:len(line)/2]
 		s_half := line[len(line)/2:]
 
 		regex := regexp.MustCompile("[" + f_half + "]")
-		sum += priorities[regex.FindStringSubmatch(s_half)[0]]
+		sum1 += priorities[regex.FindStringSubmatch(s_half)[0]]
 	}
 
-	fmt.Println("part 1: ", sum)
-	fmt.Println("part 2: ", 0)
+	sum2 := 0
+	for i := 0; i < len(lines); i += 3 {
+		regex := regexp.MustCompile("[" + lines[i] + "]")
+		common := regex.FindAllStringSubmatch(lines[i+1], -1)
+		regex = regexp.MustCompile("[" + lines[i+2] + "]")
+		common = regex.FindAllStringSubmatch(join_multi(common), -1)
+		sum2 += priorities[common[0][0]]
+	}
+
+	fmt.Println("part 1: ", sum1)
+	fmt.Println("part 2: ", sum2)
 }
