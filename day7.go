0349d526eb1f8f06bdfdbb612c2009c91c02dfbf
Bruno Loureiro <matrikss@gmail.com>
2022-12-07 09:27:25 +0000

    add day 7 part 1

---
+++ day7.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type node struct {
+	size     int
+	name     string
+	parent   *node
+	children []*node
+}
+
+func get_child_node(parent *node, folder string) *node {
+	for _, no := range parent.children {
+		if no.name == folder {
+			return no
+		}
+	}
+	return nil
+}
+
+func calculate_folder_sizes(this *node) int {
+	res := 0
+	if len(this.children) == 0 {
+		res = this.size
+	} else {
+		for _, no := range this.children {
+			res += calculate_folder_sizes(no)
+		}
+		this.size = res
+	}
+	return res
+}
+
+func main() {
+
+	input, err := os.ReadFile("./input/input7.txt")
+	check(err)
+
+	lines := strings.Split(string(input), "\r\n")
+
+	root := node{0, "/", nil, []*node{}}
+	dirs := []*node{&root}
+	current_dir := &root
+	for _, line := range lines {
+		if line[0] == '$' {
+			if line == "$ ls" {
+				// NOOP
+			} else if line[5] == '.' {
+				current_dir = current_dir.parent
+			} else {
+				current_dir = get_child_node(current_dir, strings.Split(line, " ")[2])
+			}
+		} else {
+			broken := strings.Split(line, " ")
+			size := 0
+			if broken[0] != "dir" {
+				size, _ = strconv.Atoi(broken[0])
+			}
+			new_node := node{size, broken[1], current_dir, []*node{}}
+			current_dir.children = append(current_dir.children, &new_node)
+			if broken[0] == "dir" {
+				dirs = append(dirs, &new_node)
+			}
+		}
+	}
+	calculate_folder_sizes(&root)
+
+	part1 := 0
+	for _, no := range dirs {
+		if no.size <= 100000 {
+			part1 += no.size
+		}
+	}
+
+	fmt.Println("Part 1: ", part1)
+}
