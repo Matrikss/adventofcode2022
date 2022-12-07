71ff27f4c3cc00bd32b58145abace2f1788cf1a1
Bruno Loureiro <matrikss@gmail.com>
2022-12-07 19:43:08 +0000

    add day 6 part 2

--- day6.go
+++ day6.go
@@ -6,23 +6,26 @@ import (
 	"strings"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
+// const MARKER = 4 // Part 1
+const MARKER = 14 // Part 2
 
 func main() {
 
-	input, err := os.ReadFile("./input/input6.txt")
-	check(err)
+	input, _ := os.ReadFile("./input/input6.txt")
 
 	lines := strings.Split(string(input), "\r\n")
 
 	for _, line := range lines {
 		for i := 0; i < len(line); i++ {
-			if line[i] != line[i+1] && line[i+1] != line[i+2] && line[i+2] != line[i+3] && line[i] != line[i+2] && line[i+1] != line[i+3] && line[i] != line[i+3] {
-				fmt.Println("Part1: ", i+4)
+			seen := map[byte]int{line[i]: 1}
+			for j := i + 1; j < i+MARKER; j++ {
+				if _, ok := seen[line[j]]; ok {
+					break
+				}
+				seen[line[j]] = 1
+			}
+			if len(seen) == MARKER {
+				fmt.Println("Solution: ", i+MARKER)
 				break
 			}
 		}
