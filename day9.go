08c76a04c00da9cba4e7c7bef078095a39d236be
Bruno Loureiro <matrikss@gmail.com>
2022-12-09 09:19:49 +0000

    add day 9 part 1

---
+++ day9.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type rope struct {
+	head_x         float64
+	head_y         float64
+	tail_x         float64
+	tail_y         float64
+	prev_hx        float64
+	prev_hy        float64
+	tail_positions map[string]int
+}
+
+func move_tail(corda *rope) {
+	dist_x := math.Abs(corda.tail_x - corda.head_x)
+	dist_y := math.Abs(corda.tail_y - corda.head_y)
+	//fmt.Println(dist_x, dist_y)
+	if dist_x > 1 || dist_y > 1 {
+		//fmt.Print("T:  ", corda.tail_x, corda.tail_y, " -> ")
+		corda.tail_x = corda.prev_hx
+		corda.tail_y = corda.prev_hy
+		//fmt.Println(corda.tail_x, corda.tail_y)
+		corda.tail_positions[fmt.Sprintf("%d %d", int(corda.tail_x), int(corda.tail_y))] += 1
+	}
+}
+
+func move_head(corda *rope, x_inc float64, y_inc float64) {
+	//fmt.Println("move start")
+	abs_move := math.Abs(x_inc) + math.Abs(y_inc)
+	for i := 0; i < int(abs_move); i++ {
+		corda.prev_hx = corda.head_x
+		corda.prev_hy = corda.head_y
+		corda.head_x += x_inc / abs_move
+		corda.head_y += y_inc / abs_move
+		//fmt.Println("H: ", corda.prev_hx, corda.prev_hy, "->", corda.head_x, corda.head_y)
+		//fmt.Println(corda.head_x, corda.head_y)
+		move_tail(corda)
+	}
+}
+
+func main() {
+
+	input, _ := os.ReadFile("./input/input9.txt")
+
+	lines := strings.Split(string(input), "\r\n")
+
+	corda := rope{0, 0, 0, 0, 0, 0, map[string]int{"0 0": 1}}
+	for _, line := range lines {
+		broken := strings.Split(line, " ")
+		quantity, _ := strconv.Atoi(broken[1])
+		if broken[0] == "R" {
+			move_head(&corda, float64(quantity), 0)
+		}
+		if broken[0] == "L" {
+			move_head(&corda, -float64(quantity), 0)
+		}
+		if broken[0] == "U" {
+			move_head(&corda, 0, -float64(quantity))
+		}
+		if broken[0] == "D" {
+			move_head(&corda, 0, float64(quantity))
+		}
+	}
+
+	//fmt.Println(corda.tail_positions)
+	fmt.Println("Part 1: ", len(corda.tail_positions))
+	fmt.Println("Part 2: ", 0)
+}
