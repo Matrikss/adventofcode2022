04c47543316d48b4c1372259a6f11e5160ea08f6
Bruno Loureiro <matrikss@gmail.com>
2022-12-10 09:05:36 +0000

    add day 10 part 2

--- day10.go
+++ day10.go
@@ -7,11 +7,21 @@ import (
 	"strings"
 )
 
+func print_screen(screen [40][6]string) {
+	for j := 0; j < 6; j++ {
+		for i := 0; i < 40; i++ {
+			char := screen[i][j]
+			fmt.Print(char)
+		}
+		fmt.Println()
+	}
+}
+
 func main() {
 
 	input, _ := os.ReadFile("./input/input10.txt")
 
-	var x_per_cycle [250]int
+	var x_per_cycle [241]int
 	x_per_cycle[0] = 1
 
 	lines := strings.Split(string(input), "\r\n")
@@ -34,7 +44,19 @@ func main() {
 	for i := 20; i < 225; i += 40 {
 		part1 += i * x_per_cycle[i-1]
 	}
-	//fmt.Println(x_per_cycle)
 	fmt.Println("Part 1: ", part1)
-	fmt.Println("Part 2: ", 0)
+
+	var screen [40][6]string
+	for i := 0; i < 240; i++ {
+		x := i % 40
+		y := i / 40
+		if x_per_cycle[i] <= x+1 && x_per_cycle[i] >= x-1 {
+			screen[x][y] = "#"
+		} else {
+			screen[x][y] = "_"
+		}
+	}
+
+	fmt.Println("Part 2:")
+	print_screen(screen)
 }
