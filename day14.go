b2133a56aaff5837359b391911e9905154178cdf
Bruno Loureiro <matrikss@gmail.com>
2022-12-14 22:56:26 +0000

    add day 14 part 2

--- day14.go
+++ day14.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-const TRIM = 480
+const TRIM = 300
 const X_SAND_DROP = 500
 
 func print_map(mapa [][]string) {
@@ -53,6 +53,9 @@ func add_sand(mapa [][]string) bool {
 	x_size := len(mapa)
 	y_size := len(mapa[0])
 	x := X_SAND_DROP
+	if mapa[x][0] != "" {
+		return true //if it reaches the source
+	}
 	for y := 0; y < y_size-1; y++ {
 		if mapa[x][y+1] != "" {
 			if x-1 >= 0 && mapa[x-1][y+1] == "" {
@@ -74,12 +77,13 @@ func main() {
 
 	input, _ := os.ReadFile("./input/input14.txt")
 
-	// didn't feel like parsing twice:
-	x_count := 560
-	y_count := 170
-	mapa := make([][]string, x_count)
+	// didn't feel like parsing twice 😅:
+	x_count := 700
+	y_count := 163
+	y_floor := 162
+	mapa1 := make([][]string, x_count)
 	for i := 0; i < x_count; i++ {
-		mapa[i] = make([]string, y_count)
+		mapa1[i] = make([]string, y_count)
 	}
 
 	lines := strings.Split(string(input), "\r\n")
@@ -88,21 +92,38 @@ func main() {
 		for i := 0; i < len(broken)-1; i++ {
 			a := strings.Split(broken[i], ",")
 			b := strings.Split(broken[i+1], ",")
-			mapa = draw_line(mapa, a[0], a[1], b[0], b[1])
+			mapa1 = draw_line(mapa1, a[0], a[1], b[0], b[1])
 		}
+	}
+
+	mapa2 := make([][]string, x_count)
+	for i := 0; i < x_count; i++ {
+		mapa2[i] = make([]string, y_count)
+		copy(mapa2[i], mapa1[i])
 	}
 
 	part1 := 0
 	finished := false
 	for {
-		finished = add_sand(mapa)
+		finished = add_sand(mapa1)
 		if finished {
 			break
 		}
 		part1++
 	}
-	//print_map(mapa)
+
+	draw_line(mapa2, strconv.Itoa(0), strconv.Itoa(y_floor), strconv.Itoa(x_count-1), strconv.Itoa(y_floor)) // 🤦
+	part2 := 0
+	finished = false
+	for {
+		finished = add_sand(mapa2)
+		if finished {
+			break
+		}
+		part2++
+	}
+	//print_map(mapa2)
 
 	fmt.Println("Part 1:", part1)
-	fmt.Println("Part 2:", 0)
+	fmt.Println("Part 2:", part2)
 }
