99b779ac1025a42ed40b922357f90c26ee0ad8d5
Bruno Loureiro <matrikss@gmail.com>
2022-12-21 11:57:50 +0000

    add day 21 part 2

--- day21.go
+++ day21.go
@@ -26,6 +26,53 @@ func get_part1(monkeys map[string]string, monkey string) int {
 	}
 }
 
+const FOUND = -1
+
+func get_part2(monkeys map[string]string, monkey string, humn int) int {
+	if monkey == "humn" {
+		return humn
+	}
+	broken := strings.Split(monkeys[monkey], " ")
+	if len(broken) == 1 {
+		yell, _ := strconv.Atoi(broken[0])
+		return yell
+	} else {
+		if monkey == "root" {
+			a := get_part2(monkeys, broken[0], humn)
+			b := get_part2(monkeys, broken[2], humn)
+			if a == b {
+				fmt.Println(get_part2(monkeys, broken[0], humn), "==", get_part2(monkeys, broken[2], humn))
+				return FOUND
+			}
+			//fmt.Println("\n", get_part2(monkeys, broken[0], humn), "!=\n", get_part2(monkeys, broken[2], humn))
+			return 0
+		}
+		if broken[1] == "+" {
+			return get_part2(monkeys, broken[0], humn) + get_part2(monkeys, broken[2], humn)
+		}
+		if broken[1] == "-" {
+			return get_part2(monkeys, broken[0], humn) - get_part2(monkeys, broken[2], humn)
+		}
+		if broken[1] == "*" {
+			return get_part2(monkeys, broken[0], humn) * get_part2(monkeys, broken[2], humn)
+		}
+		return get_part2(monkeys, broken[0], humn) / get_part2(monkeys, broken[2], humn)
+	}
+}
+
+func find_part2(monkeys map[string]string, start int) {
+	part2 := start
+	var res int
+	for {
+		res = get_part2(monkeys, "root", part2)
+		if res == FOUND {
+			break
+		}
+		part2++
+	}
+	fmt.Println("Part 2:", part2)
+}
+
 func main() {
 
 	input, _ := os.ReadFile("./input/input21.txt")
@@ -39,5 +86,6 @@ func main() {
 	}
 
 	fmt.Println("Part 1:", get_part1(monkeys, "root"))
-	fmt.Println("Part 2:", 0)
+
+	find_part2(monkeys, 3759566890000) // 3759566892641 👀🤚
 }
